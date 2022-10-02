9bee2afcb7bab20464f55ba258a50d4d2075fb06
Max Kottong <max.kottong@gmail.com>
2022-10-02 18:07:14 -0400

    Thought model

---
+++ models/Thought.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const dayjs = require('dayjs');
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: 280,
+        min: 1
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dayjs(createdAtVal).format('MM/DD/YYYY hh:mmA')
+    }
+})
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        max: 280,
+        min: 1
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dayjs(createdAtVal).format('MM/DD/YYYY hh:mmA')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [replySchema]
+})
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
