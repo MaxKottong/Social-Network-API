3493b06e5e244e5981315f7cc9d16330bc57cf9b
Max Kottong <max.kottong@gmail.com>
2022-10-02 17:58:15 -0400

    User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/.test(v)
+            },
+            message: e => `${e.value} is not a valid email address`
+        }
+    },
+    thoughts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'thought'
+    }],
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'thought'
+    }]
+})
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
