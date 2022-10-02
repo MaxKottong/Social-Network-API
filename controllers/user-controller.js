a085897c34ed84c769fcada46f77460f9aa429ee
Max Kottong <max.kottong@gmail.com>
2022-10-02 18:23:06 -0400

    user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,74 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+            .populate('thoughts')
+            .populate('friends')
+            .then(users => res.json(users))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    getUser({ params }, res) {
+        User.findOne({ _id: params.userId })
+            .populate('thoughts')
+            .populate('friends')
+            .then(userData => {
+                !userData
+                    ? res.status(404).json({ message: 'No user found with this id' })
+                    : res.json(userData);
+            })
+        .catch(err => res.json(err));
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then(userData => res.json(userData))
+            .catch(err => res.json(err))
+    },
+
+    updateUser({ params, body }, res) {
+        User.findByIdAndUpdate({ _id: params.userId }, { $set: body }, { new: true, runValidators: true })
+            .then(userData => {
+                !userData
+                    ? res.status(404).json({ message: 'No user found with this id' })
+                    : res.json(userData);
+            })
+            .catch(err => res.json(err))
+    },
+
+    deleteUser({ params }, res) {
+
+    },
+
+    addFriend({ params }, res) {
+        User.findByIdAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { new: true }
+        )
+            .then(userData => {
+                !userData
+                    ? res.status(404).json({ message: 'Failed to Add Friend: No user found with this id' })
+                    : res.json(userData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    deleteFriend({ params }, res) {
+        User.findByIdAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true }
+        )
+            .then(userData => {
+                !userData
+                    ? res.status(404).json({ message: 'Failed to Add Friend: No user found with this id' })
+                    : res.json(userData);
+            })
+            .catch(err => res.json(err));
+    }
+}
